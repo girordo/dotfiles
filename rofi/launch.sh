032c4eff9b96d8390aa8be671737f2983fb4ffc3
Tarcísio Giroldo <tsgiroldo@gmail.com>
2022-01-26 14:55:41 -0300

    :bug: | Fixing $HOME in rofi config file

--- rofi/launch.sh
+++ rofi/launch.sh
@@ -2,7 +2,7 @@
 #! /usr/bin/env bash
 
 # launch apps menu
-rofi -theme .config/rofi/themes/main.rasi -show drun
+rofi -theme $HOME/.config/rofi/themes/main.rasi -show drun
 
 # launch window menu
 # rofi -theme $HOME/.config/rofi/themes/main.rasi -show window
