0108600e1a04d2816c8be7df39f6c1f3ef5b07b1
girordo <tsgiroldo@gmail.com>
2023-08-20 23:24:38 -0300

    feat(nvim): nvim config custom for nvchad added

---
+++ nvim/configs/lspconfig.lua
@@ -0,0 +1,20 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "html", "cssls", "clangd", "jsonls", "tsserver", "unocss", "bashls", "pylsp" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig.rust_analyzer.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "rust" },
+}
+
+
---
+++ nvim/plugins.lua
@@ -0,0 +1,99 @@
+local overrides = require("custom.configs.overrides")
+
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Override plugin definition options
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      overrides.mason,
+      ensure_installed = {
+        "rust-analyzer",
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+
+        -- web dev
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "emmet-ls",
+        "json-lsp",
+        "prettier",
+        "eslint-lsp",
+
+        -- shell
+        "bash-language-server",
+        "shfmt",
+        "shellcheck",
+
+        -- python
+        "pyright",
+        "black",
+
+        -- terraform
+        "terraform-ls",
+
+        -- ansible
+        "ansible-language-server",
+        
+        -- docker
+        "dockerfile-language-server"
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+  -- To make a plugin not be loaded
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
+  -- },
+
+}
+
+return plugins
