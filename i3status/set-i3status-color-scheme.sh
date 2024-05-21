51fd431a4f93632958757e7b09e8628664d4e754
girordo <tsgiroldo@gmail.com>
2024-05-21 18:21:59 -0300

    feat(i3, kitty, and other stuff): new files

---
+++ i3status/set-i3status-color-scheme.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+# Author:		Arturo Aguilar Lagunas
+# Date:			Fri 10 Sep 2021 01:46:57 AM CDT
+# Description:	Set xresources color scheme to i3status
+
+config=$HOME/.config/i3status/config
+c10=$($HOME/.homemade/bin/system/get-xresources-color color10)
+c13=$($HOME/.homemade/bin/system/get-xresources-color color13)
+c9=$($HOME/.homemade/bin/system/get-xresources-color color9)
+
+sed --follow-symlinks -i 's/^.*color_good.*#[a-fA-F0-9]\{6\}"/        color_good = "'$c10'"/' $config
+sed --follow-symlinks -i 's/^.*color_degraded.*#[a-fA-F0-9]\{6\}"/        color_degraded = "'$c13'"/' $config
+sed --follow-symlinks -i 's/^.*color_bad.*#[a-fA-F0-9]\{6\}"/        color_bad = "'$c9'"/' $config
